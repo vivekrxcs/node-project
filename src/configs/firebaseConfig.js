8e36a476ed83e85c310151f3d473630f1eff31ea
Vivek <vivek.kumar@rxcs.in>
2022-12-21 16:18:44 +0530

    first commit

---
+++ src/configs/firebaseConfig.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
+//import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHufixGzYz7gbanI7TUjIGzPihWMIPn78",
+  authDomain: "chatapp-365c2.firebaseapp.com",
+  databaseURL: "https://chatapp-365c2-default-rtdb.firebaseio.com",
+  projectId: "chatapp-365c2",
+  storageBucket: "chatapp-365c2.appspot.com",
+  messagingSenderId: "1013092125286",
+  appId: "1:1013092125286:web:8e0ddccf2c0d19b179fba7",
+  measurementId: "G-H3LDETMLC0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const storage = getStorage(app);
+
+export {db, storage};
+
+//const analytics = getAnalytics(app);
---
+++ src/pages/about/index.js
@@ -0,0 +1,9 @@
+const about = () => {
+  
+  return (
+    <div>
+       Chat page here
+    </div>
+  )
+}
+export default about
